d91dc726f17a6d191c3fe5ab44976521204e49d6
anilsayar <anilsayar@yahoo.com>
2018-12-18 21:44:16 +0100

    gitignore added!

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -9,7 +9,7 @@ public class LoginTest extends BaseWebTest {
     @Test
     public void LoginTest(){
         HomePage home = new HomePage(driver);
-        home.Login("user eMail","user Pass");
+        home.Login("user eMail","user Pass pls!");
         home.checkLogin();
         home.LogOut();
         home.checkLogOut();
